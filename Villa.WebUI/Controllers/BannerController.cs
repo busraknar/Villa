9335050f2ee5d04f9e9578f6fd0dbc57c3932cac
busraknar <b.ertas91@gmail.com>
2024-04-09 01:18:18 +0300

    adminLayout is created

--- Villa.WebUI/Controllers/BannerController.cs
+++ Villa.WebUI/Controllers/BannerController.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Villa.Business.Abstract;
+using Villa.Dto.Dtos.BannerDtos;
 
 namespace Villa.WebUI.Controllers
 {
     public class BannerController : Controller
     {
         private readonly IBannerService _bannerService;
+        private readonly IMapper _mapper;
 
-        public BannerController(IBannerService bannerService)
+        public BannerController(IBannerService bannerService, IMapper mapper)
         {
             _bannerService = bannerService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
-            return View();
+            var values = await _bannerService.TGetListAsync();
+            var bannerList = _mapper.Map<List<ResultBannerDto>>(values);
+            return View(bannerList);
         }
     }
 }
